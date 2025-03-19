577977710a30ff6fb2d3291dde5b105db4796238
nisaladriberge <nisala.20230320@iit.ac.lk>
2025-03-19 20:23:22 +0530

    updating the checkout page to insert the details of the ordered items in the database tables and display a final confirmation

--- Checkout.php
+++ Checkout.php
@@ -1,32 +1,60 @@
-<?php //template page
+<?php
 session_start();
 include("db.php");
 mysqli_report(MYSQLI_REPORT_OFF);
-$pagename="template"; //Create and populate a variable called $pagename
+$pagename = "checkout";
 
-echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
-echo "<title>".$pagename."</title>"; //display name of the page as window title
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+echo "<title>" . $pagename . "</title>";
 echo "<body>";
-include ("headfile.html"); //include header layout file
-include ("detectlogin.php");
+include("headfile.html");
+include("detectlogin.php");
 
-echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
-/store the current date and time in a local variable $currentdatetime
+echo "<h4>" . $pagename . "</h4>";
+
 $currentdatetime = date('Y-m-d H:i:s');
-//write a SQL query to insert a new record in the Orders table to generate a new order.
-$SQL = "INSERT into Orders (userId, orderDateTime, orderStatus)
-VALUES ('".$_SESSION['userid']."','".$currentdatetime."', 'Placed')";
-//if execution of the INSERT INTO SQL query to add new order is correct
-if (mysqli_query($conn, $SQL))
-{
-//Display "order success" message
-echo "<p><b>Order successfully placed!</b></p>";
+$SQL = "INSERT INTO Orders (userId, orderDateTime, orderStatus) VALUES ('" . $_SESSION['userid'] . "', '" . $currentdatetime . "', 'Placed')";
+
+if (mysqli_query($conn, $SQL) && isset($_SESSION['basket']) && count($_SESSION['basket']) > 0) {
+    echo "<p><b>Order successfully placed!</b></p>";
+    
+    $maxSQL = "SELECT MAX(orderNo) AS maxOrderNo FROM Orders WHERE userId = " . $_SESSION['userid'];
+    $exemaxSQL = mysqli_query($conn, $maxSQL) or die(mysqli_error($conn));
+    $arrayo = mysqli_fetch_array($exemaxSQL);
+    $orderno = $arrayo['maxOrderNo'];
+    
+    echo "<p>Order No: <b>" . $orderno . "</b></p>";
+    
+    $total = 0;
+    echo "<table id='checkouttable'>";
+    echo "<tr><th>Product name</th><th>Price</th><th>Quantity</th><th>Subtotal</th></tr>";
+    
+    foreach ($_SESSION['basket'] as $index => $value) {
+        $SQLb = "SELECT prodId, prodName, prodPrice FROM Product WHERE prodId = " . $index;
+        $exeSQLb = mysqli_query($conn, $SQLb) or die(mysqli_error($conn));
+        $arrayb = mysqli_fetch_array($exeSQLb);
+        
+        $subtotal = $value * $arrayb['prodPrice'];
+        
+        $SQLol = "INSERT INTO Order_Line (orderNo, prodId, quantityOrdered, subTotal) VALUES ('" . $orderno . "', '" . $index . "', '" . $value . "', '" . $subtotal . "')";
+        mysqli_query($conn, $SQLol) or die(mysqli_error($conn));
+        
+        echo "<tr><td>" . $arrayb['prodName'] . "</td><td>&pound" . $arrayb['prodPrice'] . "</td><td>" . $value . "</td><td>&pound" . $subtotal . "</td></tr>";
+        
+        $total += $subtotal;
+    }
+    
+    echo "<tr><th colspan='3'>TOTAL</th><th>&pound" . $total . "</th></tr>";
+    echo "</table>";
+    
+    $SQLo = "UPDATE Orders SET orderTotal = " . $total . " WHERE orderNo = " . $orderno;
+    mysqli_query($conn, $SQLo) or die(mysqli_error($conn));
+    
+    unset($_SESSION['basket']);
+} else {
+    echo "<p><b>Error with the placing of your order!</b></p>";
 }
-else
-{
-//Display "order error" message
-echo "<p><b>Error with the placing of your order!</b></p>";
-}
-include("footfile.html"); //include head layout
+
+include("footfile.html");
 echo "</body>";
 ?>
